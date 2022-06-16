e9188dc2605542d59dac3a6521cf6825635d1512
Genaro OG <genaro.osorio@icoddes.com>
2022-06-16 01:39:38 -0500

    usando fetch y mostrando titulo e imagen

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,17 +1,40 @@
 import { useState, useEffect } from 'react'
+import GifGridItem from './GifGridItem'
 import { getGifs } from '../helpers/getGifs'
 
 const GifGrid = ({ category }) => {
 
   const [images, setImages] = useState([])
 
+  const getImages = async() => {
+    const newImages = await getGifs(category)
+
+    setImages(newImages)
+  }
+
   useEffect(() => {
-    getGifs(category)
+    // getGifs(category)
+    //   .then( newImages => setImages(newImages) )
+
+    getImages()
   }, [])
   
 
   return (
-    <h3>{category}</h3>
+    <>
+      <h3>{category}</h3>
+  
+      <div className='card-grid'>
+        {
+          images.map( image => (
+            <GifGridItem 
+              key={image.id} 
+              { ...image }
+            />
+          ))
+        }
+      </div>
+    </>
   )
 }
 
